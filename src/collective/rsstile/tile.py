c6abf5ef9d96eec3fcd6229e1c3d7383cd1078b5
Matthew Wilkes <git@matthewwilkes.name>
2014-06-18 23:11:55 +0100

    Working first version

--- src/collective/rsstile/tile.py
+++ src/collective/rsstile/tile.py
@@ -1,10 +1,17 @@
-from zope.interface import Interface
+import operator
+from itertools import ifilter, islice
+
 from zope.interface import implements
-from zope.schema import List, URI, Int, Bytes
+from zope.schema import List, URI, Int, ASCII
 from zope.schema._compat import u
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.portlets.portlets.rss import RSSFeed
+from Products.CMFCore.utils import getToolByName
 
 from collective.cover.tiles.base import IPersistentCoverTile, PersistentCoverTile
+
+# Copy approach from RSS Portlet
+FEED_DATA = {}
 
 class IRSSTileData(IPersistentCoverTile):
 
@@ -25,7 +32,7 @@ class IRSSTileData(IPersistentCoverTile):
     blacklist = List(
         title=u("Blacklisted words"),
         unique=True,
-        value_type=Bytes(title=u('Word')),
+        value_type=ASCII(title=u('Word')),
         required=False,
         missing_value=[],
     )
@@ -33,7 +40,7 @@ class IRSSTileData(IPersistentCoverTile):
     whitelist = List(
         title=u("Whitelisted words"),
         unique=True,
-        value_type=Bytes(title=u('Word')),
+        value_type=ASCII(title=u('Word')),
         required=False,
         missing_value=[],
     )
@@ -47,14 +54,68 @@ class RSSTile(PersistentCoverTile):
     short_name = "RSS"
     
     index = ViewPageTemplateFile('tile.pt')
+    
+    def get_feeds(self):
+        for feed in self.data['feeds']:
+            feed_obj = FEED_DATA.get(feed, None)
+            if feed_obj is None:
+                feed_obj = FEED_DATA[feed] = RSSFeed(feed, 120)
+            yield feed_obj
+    
+    def matches_whitelist(self, item):
+        if not self.data["whitelist"]:
+            # No whitelist implies a match
+            return True
         
+        text = item.get('title', '') + " " + item.get('summary', '')
+        text = text.lower()
+        for word in self.data['whitelist']:
+            word = word.lower()
+            if word in text:
+                return True
+        return False
+    
+    def blacklist_permits(self, item):
+        if not self.data["blacklist"]:
+            # No blacklist implies acceptability
+            return True
+        
+        text = item.get('title', '') + " " + item.get('summary', '')
+        text = text.lower()
+        for word in self.data['blacklist']:
+            word = word.lower()
+            if word in text:
+                return False
+        return True
+    
+    def fmt(self, date):
+        return "(%s)" % date.strftime("%Y-%m-%d")
+    
     def get_items(self):
         items = []
         if self.data['feeds'] is None:
             # We're unconfigured!
             return []
         
-        for feed in self.data['feeds']:
-            items.append({'title': feed})
+        total_slots = self.data.get("items_to_show", 4)
+        total_per_feed = self.data.get("max_per_feed", 1)
         
+        
+        transformer = getToolByName(self.context, 'portal_transforms', None)
+        convert = lambda text: unicode(transformer.convertTo("text/plain", text, mimetype="text/html")) or text
+                                
+        items = []
+        for feed in self.get_feeds():
+            feed.update()
+            for item in islice(
+                            ifilter(self.matches_whitelist, ifilter(self.blacklist_permits, feed.items)),
+                            0, total_per_feed):
+                if transformer is not None:
+                    item['title'] = convert(item['title'])
+                    item['summary'] = convert(item['summary'])
+                items.append(item)
+        
+        items = sorted(items, key=operator.itemgetter('updated'))
+        items = items[0:total_slots]
         return items
+    
